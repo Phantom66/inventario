a5b8180fc60f98bdcfdb91733616b5d92dfefc67
Phantom66 <leoturorellana@gmail.com>
2018-02-22 16:02:43 -0400

    Agrupando las rutas para que serán validadas por el usuario

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,22 @@
 
 
 
-Route::get('/', 'BienNacionalController@index')->name('inventario_path');
+Route::group(['middleware' => 'auth'],function(){
 
-Route::get('inventario/create', 'BienNacionalController@create')->name('inventario_create_path');
+	Route::get('/', 'BienNacionalController@index')->name('inventario_path');
 
-Route::post('inventario/store', 'BienNacionalController@store')->name('inventario_store_path');
+	Route::get('inventario/create', 'BienNacionalController@create')->name('inventario_create_path');
 
-Route::get('inventario/{bien}', 'BienNacionalController@show')->name('inventario_path');
+	Route::post('inventario/store', 'BienNacionalController@store')->name('inventario_store_path');
 
-Route::get('inventario/{bien}/edit', 'BienNacionalController@edit')->name('inventario_edit_path');
+	Route::get('inventario/{bien}', 'BienNacionalController@show')->name('inventario_path');
 
-Route::delete('inventario/{bien}', 'BienNacionalController@destroy')->name('inventario_delete_path');
+	Route::get('inventario/{bien}/edit', 'BienNacionalController@edit')->name('inventario_edit_path');
+
+	Route::delete('inventario/{bien}', 'BienNacionalController@destroy')->name('inventario_delete_path');
+
+});
+
 
 
 Auth::routes();
