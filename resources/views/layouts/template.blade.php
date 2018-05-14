53ab9bd1e5289dd3adbb459b06b10237931f5611
Phantom66 <leoturorellana@gmail.com>
2018-05-13 21:34:31 -0400

    carpinteria

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -8,10 +8,6 @@
       <!-- CSRF Token -->
    <meta name="csrf-token" content="{{ csrf_token() }}">
    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles  <link href="{{ asset('css/app.css') }}" rel="stylesheet">-->                       
- 
-
   <!-- Bootstrap core CSS-->
   <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
